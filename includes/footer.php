7b92d086fbfa6c727d2724102b3fcedf466cf8d0
kuku martens <princegutierrez1004@gmail.com>
2026-03-26 23:23:34 +0800

    Delete includes directory

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-    <?php
-    $inPages = strpos($_SERVER['PHP_SELF'], '/pages/') !== false;
-    $jsPath = $inPages ? '../assets/js/main.js' : 'assets/js/main.js';
-    ?>
-    <script src="<?php echo $jsPath; ?>"></script>
-</body>
-</html>
